762d7778576f63ff941a3121fd131f6247416719
Mikyung Shin <mkshin920@gmail.com>
2021-03-27 23:48:29 +0900

    Login Button(setState)

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,6 +3,7 @@ import {
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import styled from "styled-components";
 import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
@@ -24,17 +25,41 @@ const FacebookLogin = styled.div`
   }
 `;
 function Login() {
+  const [username, setUsername] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const onUsernameChange = (event) => {
+    setUsernameError("");
+    setUsername(event.target.value);
+  };
+  const HandleSubmit = (event) => {
+    event.preventDefault();
+    if (username === "") {
+      setUsernameError("empty");
+    }
+    if (username.length < 10) {
+      setUsernameError("too short");
+    }
+  };
   return (
     <AuthLayout>
       <FormBox>
         <div>
           <FontAwesomeIcon icon={faInstagram} size="3x" />
         </div>
-        <h1>Instagram</h1>
-        <form>
-          <Input type="text" placeholder="Username" />
+        <form onSubmit={HandleSubmit}>
+          {usernameError}
+          <Input
+            onChange={onUsernameChange}
+            value={username}
+            type="text"
+            placeholder="Username"
+          />
           <Input type="password" placeholder="Password" />
-          <Button type="submit" value="Log in" />
+          <Button
+            type="submit"
+            value="Log in"
+            disabled={username === "" && username.length < 10}
+          />
         </form>
         <Seperator />
         <FacebookLogin>
