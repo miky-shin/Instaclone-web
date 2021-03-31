69d95288af81c6b30e9f95ae271df3feb017ffe4
Mikyung Shin <mkshin920@gmail.com>
2021-03-31 09:47:37 +0900

    create Account

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,13 +1,18 @@
+import { useMutation } from "@apollo/client";
 import {
   faFacebookSquare,
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import gql from "graphql-tag";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router";
 import styled from "styled-components";
 import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
 import Button from "../components/auth/Button";
 import FormBox from "../components/auth/FormBox";
+import FormError from "../components/auth/FormError";
 import Input from "../components/auth/Input";
 import Seperator from "../components/auth/Seperator";
 import PageTitle from "../components/PageTitle";
@@ -48,11 +53,58 @@ const FacebookLogin = styled.div`
     margin-left: 7px;
   }
 `;
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
 
 function SignUp() {
+  const history = useHistory();
+  const onCompleted = (data) => {
+    const {
+      createAccount: { ok, error },
+    } = data;
+    if (!ok) {
+      return;
+    }
+    history.push(routes.home);
+  };
+  const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, {
+    onCompleted,
+  });
+  const { register, handleSubmit, errors, formState } = useForm({
+    mode: "onChange",
+  });
+  const onSubmitValid = (data) => {
+    if (loading) {
+      return;
+    }
+    createAccount({
+      variables: {
+        ...data,
+      },
+    });
+  };
+
   return (
     <AuthLayout>
-      <PageTitle title="Sign up"/>
+      <PageTitle title="Sign up" />
       <FormBox>
         <div>
           <FontAwesomeIcon icon={faInstagram} size="3x" />
@@ -66,12 +118,50 @@ function SignUp() {
           <span>Log in with Facebook</span>
         </FacebookLogin>
         <Seperator />
-        <form>
-          <Input type="text" placeholder="Mobile Number or Email" />
-          <Input type="text" placeholder="Full Name" />
-          <Input type="text" placeholder="Username" />
-          <Input type="password" placeholder="Password" />
-          <Button type="submit" value="Sign up" />
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            ref={register({ required: "First Name is required." })}
+            name="firstName"
+            type="text"
+            placeholder="First Name"
+            hasError={Boolean(errors?.firstName?.message)}
+          />
+          <FormError message={errors?.firstName?.message} />
+          <Input
+            ref={register({})}
+            name="lastName"
+            type="text"
+            placeholder="Last Name"
+          />
+          <Input
+            ref={register({ required: "Email is required." })}
+            name="email"
+            type="text"
+            placeholder="Email"
+            hasError={Boolean(errors?.email?.message)}
+          />
+          <FormError message={errors?.email?.message} />
+          <Input
+            ref={register({ required: "Username is required." })}
+            name="username"
+            type="text"
+            placeholder="Username"
+            hasError={Boolean(errors?.username?.message)}
+          />
+          <FormError message={errors?.username?.message} />
+          <Input
+            ref={register({ required: "Password is required." })}
+            name="password"
+            type="password"
+            placeholder="Password"
+            hasError={Boolean(errors?.password?.message)}
+          />
+          <FormError message={errors?.password?.message} />
+          <Button
+            type="submit"
+            value={loading ? "Loading..." : "Sign up"}
+            disabled={!formState.isValid || loading}
+          />
         </form>
         <Description2>
           <span>
