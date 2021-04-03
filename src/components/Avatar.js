5475cd70eb77cd600709657d4abac0d02952b247
Mikyung Shin <mkshin920@gmail.com>
2021-04-03 10:20:59 +0900

    avatar

---
+++ src/components/Avatar.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const SAvatar = styled.div`
+  width: 18px;
+  height: 18px;
+  border-radius: 15px;
+  background-color: #2c2c2c;
+  overflow: hidden;
+`;
+const Img = styled.img`
+  max-width: 100%;
+`;
+
+function Avatar({ url = "" }) {
+  return <SAvatar>{url !== "" ? <Img src={url} /> : null}</SAvatar>;
+}
+export default Avatar;
